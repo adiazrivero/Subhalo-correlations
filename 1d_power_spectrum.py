0e45c6145447687ffab11eb134e1f2dab6876a77
adiazrivero <32441214+adiazrivero@users.noreply.github.com>
2017-11-01 14:01:53 -0400

    Add files via upload

--- 1d_power_spectrum.py
+++ 1d_power_spectrum.py
@@ -4,43 +4,84 @@ import matplotlib.pyplot as plt
 import pylab as py
 import sys
 import time
-from test import test
-from functions import simul_data,rotation,projections,twoD_ps,oneD_ps,error_bars
+from functions import simul_data,rotation,projections,twoD_ps,angular_average,variance,poisson_realization
 
 start_time = time.time()
 
 rnge = 100
+list1 = ['fof_subhalo_tab_127.0.hdf5','fof_subhalo_tab_127.1.hdf5','fof_subhalo_tab_127.2.hdf5','fof_subhalo_tab_127.3.hdf5','fof_subhalo_tab_127.4.hdf5','fof_subhalo_tab_127.5.hdf5','fof_subhalo_tab_127.6.hdf5','fof_subhalo_tab_127.7.hdf5','fof_subhalo_tab_127.8.hdf5','fof_subhalo_tab_127.9.hdf5','fof_subhalo_tab_127.10.hdf5','fof_subhalo_tab_127.11.hdf5','fof_subhalo_tab_127.12.hdf5','fof_subhalo_tab_127.13.hdf5','fof_subhalo_tab_127.14.hdf5','fof_subhalo_tab_127.15.hdf5']
 
-subh_mass,subh_pos = simul_data(Rnge=600)
+# SIMULATION DATA
 
-coords,avg_num_subh = projections(subh_pos,rnge=rnge,shift=0,num_proj=1000) 
+data = True
+if data == True:
+    subh_mass,subh_pos = simul_data(list1,Rnge=600)
+    coords,avg_num_subh = projections(subh_pos,rnge=rnge,shift=0,num_proj=1000)
+    individual_ps,tot_ps,kx,ky,bin_size = twoD_ps(coords,bns=21,rnge=rnge,shift=0,show_nr=False,show_ps=False)
+    pix_size_k = np.abs(kx[0]-kx[1])
+    ps1d,K,norm = angular_average(tot_ps,kx,ky,rnge=rnge,bin_size=bin_size,dk=pix_size_k)
+    var = variance(individual_ps,ps1d,len(coords),kx,ky,rnge=rnge,pix_size=pix_size_k)
+    std = [np.sqrt(i) for i in var]
+    #std = None
 
-#tst = test(rnge=rnge,shift=0,subh_num=avg_num_subh,show_nr=False,show_ps=False)
-tst = None
+    print "P_ss(k) = %s " % ps1d
+    print "var= %s" % var
+    print "std= %s" % std
 
-coadd_ps,tot_ps,kx,ky,bin_size = twoD_ps(coords,bns=20,rnge=rnge,shift=0,show_nr=False,show_ps=False)
+# POISSON-DISTRIBUTED REALIZATIONS
 
-pix_size_k = np.abs(kx[0]-kx[1])
-ps1d,K,norm = oneD_ps(tot_ps,kx,ky,rnge=rnge,bin_size=bin_size,pixel_size=pix_size_k)
+test = True
+if test == True:
+    """vals = [0.3,0.5,1,2,3]
+    for x in vals:
+        print "_____________________________"
+        print x
+        num_subh = avg_num_subh*x
+        print "average number of subhalos on the plane (poisson): %s" % num_subh
+        poiss_coords = poisson_realization(rnge=rnge,subh_num=num_subh,num_proj=3000)
+        individual_ps2,tot_ps2,kx2,ky2,bin_size2 = twoD_ps(poiss_coords,bns=21,rnge=rnge,shift=0,show_nr=False,show_ps=False)
 
-error = error_bars(coadd_ps,ps1d,kx,ky,rnge=rnge,pix_size=pix_size_k)
-#error = None
+        pix_size_k2 = np.abs(kx[0]-kx[1])
+        noise,K2,norm2 = angular_average(tot_ps2,kx2,ky2,rnge=rnge,bin_size=bin_size,dk=pix_size_k2)
+
+        print noise
+        var2 = variance(individual_ps2,noise,len(poiss_coords),kx2,ky2,rnge=rnge,pix_size=pix_size_k2)
+        std2 = [np.sqrt(i) for i in var]
+        #std2 = None
+        print "var= %s" % var2
+        print "std= %s" % std2"""
+
+    num_subh = avg_num_subh
+    print "average number of subhalos on the plane (poisson): %s" % num_subh
+    poiss_coords = poisson_realization(rnge=rnge,subh_num=num_subh,num_proj=3000)
+    individual_ps2,tot_ps2,kx2,ky2,bin_size2 = twoD_ps(poiss_coords,bns=21,rnge=rnge,shift=0,show_nr=False,show_ps=False)
+    pix_size_k2 = np.abs(kx[0]-kx[1])
+    noise,K2,norm2 = angular_average(tot_ps2,kx2,ky2,rnge=rnge,bin_size=bin_size,dk=pix_size_k2)
+    var2 = variance(individual_ps2,noise,len(poiss_coords),kx2,ky2,rnge=rnge,pix_size=pix_size_k2)
+    std2 = [np.sqrt(i) for i in var2]
+    #std2 = None
+
+    print "noise PS = %s " % noise
+    print "var = %s" % var2
+    print "std = %s" % std2
 
 py.figure('1d Power Spectrum')
 ax = plt.subplot(111)
 ax.set_xscale("log")
 ax.set_yscale("log")
-if error is not None:
-    plt.loglog(K,ps1d)
-else:
-    plt.errorbar(K, ps1d, yerr=error)
-if tst is not None:
-    plt.loglog(K,tst)
-#ax.set_xlim(min(K)-1e-1,max(K)+1e-1)
-ax.set_ylim(1e-1,max(ps1d)+1)
+if data == True:
+    if std is None:
+        plt.plot(K,ps1d,label='P_ss(k)')
+    else:
+        plt.errorbar(K, ps1d, yerr=std,label='P_ss(k)')
+if test == True:
+    if std2 is None:
+        plt.plot(K2,noise,label='P_noise(k)')
+    else:
+        plt.errorbar(K2, noise, yerr=std2,label='P_noise(k)')
 ax.set_xlabel('k [h kpc]^-1')
-ax.set_ylabel('P_ss(k)[kpc/h]^2')
+ax.set_ylabel('P(k)[kpc/h]^2')
+plt.legend()
 plt.show()
-
 
 print("--- %s seconds ---" % (time.time() - start_time))
--- functions.py
+++ functions.py
@@ -5,13 +5,21 @@ import pylab as py
 import h5py
 import sys
 
-def simul_data(Rnge=1):
-    list1 = ['fof_subhalo_tab_127.0.hdf5','fof_subhalo_tab_127.1.hdf5','fof_subhalo_tab_127.2.hdf5','fof_subhalo_tab_127.3.hdf5','fof_subhalo_tab_127.4.hdf5','fof_subhalo_tab_127.5.hdf5','fof_subhalo_tab_127.6.hdf5','fof_subhalo_tab_127.7.hdf5','fof_subhalo_tab_127.8.hdf5','fof_subhalo_tab_127.9.hdf5','fof_subhalo_tab_127.10.hdf5','fof_subhalo_tab_127.11.hdf5','fof_subhalo_tab_127.12.hdf5','fof_subhalo_tab_127.13.hdf5','fof_subhalo_tab_127.14.hdf5','fof_subhalo_tab_127.15.hdf5']
 
+def simul_data(data,Rnge=1):
+    """
+    takes in simulation data and returns subhalo masses and positions
+    inputs:
+        data: simulation data
+        Rnge: (2*radial distance) out to which we want to include subhalos
+    outputs:
+        subh_mass: list of subhalo masses
+        subh_pos: list of subhalo positions
+    """
     masses = []
     positions1 = []
     vmax1 = []
-    for i in list1:
+    for i in data:
         file = h5py.File(i,'r')
         if len(file['Subhalo'].keys()) != 0:
             masses.append(file['Subhalo']['SubhaloMass'].value) #in 10^10 M_sun/h
@@ -28,16 +36,33 @@ def simul_data(Rnge=1):
     [subh_cut.append(i) for i in subh if i[0] > 1.5e-4 and -Rnge/2. < i[1][0] < Rnge/2. and -Rnge/2. < i[1][1] < Rnge/2. and -Rnge/2. < i[1][2] < Rnge/2.]
 
     subh_mass,subh_pos = zip(*subh_cut)
-    #print "total number of ETHOS subhalos within r < %s kpc/h: %s " % (Rnge/2,len(subh_cut))
 
     return subh_mass,subh_pos
 
 def rotation(nx,ny,nz,theta):
+    """
+    input:
+        unit vector (nx,ny,nz)
+        theta: rotation angle
+    output:
+        3x3 rotation matrix that rotates a 3d vector about a unit vector (nx,ny,nz) by an angle theta
+    """
     R = [[np.cos(theta) + (nx**2)*(1-np.cos(theta)) , nx*ny*(1-np.cos(theta)) - nz*np.sin(theta) , nx*nz*(1-np.cos(theta)) + ny*np.sin(theta)], [nx*ny*(1-np.cos(theta)) + nz*np.sin(theta) , np.cos(theta) + (ny**2)*(1-np.cos(theta)) , ny*nz*(1-np.cos(theta)) - nx*np.sin(theta)], [nz*nx*(1-np.cos(theta)) - ny*np.sin(theta) , nz*ny*(1-np.cos(theta)) + nx*np.sin(theta) , np.cos(theta) + (nz**2)*(1-np.cos(theta))]]
 
     return R
 
 def projections(positions,rnge=100,shift=0,num_proj=1000):
+    """
+    takes in arrays of 3d positions and returns arrays of 2d positions
+    inputs:
+        positions: array of 3d positions
+        rnge: (2*radial distance) out to which we want to include subhalos (i.e. box size)
+        shift: optional shift away from the host center; default is zero
+        num_proj: (total number of 2d maps we want)/3 (the division by three is because for each 3d map we project in three different directions, namely xy,xz,yz)
+    outputs:
+        coords: array of 2d positions
+        avg_num_subh: average number of subhalos within r < rnge/2 kpc/h after rotating and projecting
+    """
     coords = []
     count = 0
     while count < num_proj:
@@ -66,7 +91,8 @@ def projections(positions,rnge=100,shift=0,num_proj=1000):
         for i in coords:
             tot_num_subh.append(len(i))
         avg_num_subh = np.mean(tot_num_subh)
-        #print "average number of subhalos within r < 50 kpc/h after rotating & projecting: %s" % np.mean(tot_num_subh)
+
+    #print "average number of subhalos within r < 50 kpc/h after rotating & projecting: %s" % np.mean(tot_num_subh)
 
     return coords,avg_num_subh
 
@@ -74,31 +100,39 @@ def projections(positions,rnge=100,shift=0,num_proj=1000):
 def twoD_ps(coords,bns=20,rnge=100,shift=0,show_nr=False,show_ps=False):
     """
     takes in projected subhalo positions and returns the 2D power spectrum:
-        coords: array of 2d subhalo positions
+    inputs:
+        coords: list of lists, where each list is an array of 2d subhalo positions
         bns: number of bins in the horizontal and vertical axes of the 2D power spectrum
-        rnge: spatial range of the subhalo positions
+        rnge: box size
         shift: offset from the halo center
-        show_nr: whether you want to see xi_ss
-        show_ps: whether you want to see the 2d power spectrum
+        show_nr: whether you want to display xi_ss
+        show_ps: whether you want to display the 2d power spectrum
+    outputs: individual_ps,tot_ps,kx,ky,bin_size
+        individual_ps: list of lists, where each list is a 2d power spectrum
+        tot_ps: total 2D power spectrum after coadding all 2d power spectra
+        ky,ky: fft frequencies
+        bin_size: pixel size
     """
     bin_size = rnge/bns
     bin_avg = []
-    coadd_ps = []
+    individual_ps = []
     coadd_corr = []
 
     for i in coords:
         x,y = zip(*i)
-        H,xedges,yedges = np.histogram2d(x,y,bins=bns)
-        Nbar = H.mean()
+        N,xedges,yedges = np.histogram2d(x,y,bins=bns)
+        Nbar = N.mean()
         bin_avg.append(Nbar)
-        corr = (H - Nbar) / Nbar
+        corr = (N - Nbar) / Nbar
         coadd_corr.append(corr)
         ft = np.fft.fft2(corr)
         ft = 1/(2*np.pi)*np.fft.fft2(corr)
         ps2D = np.abs(ft)**2
-        coadd_ps.append(ps2D)
+        individual_ps.append(ps2D)
 
-    #print "average nbar: %s " % np.mean(bin_avg)
+    print "average Nbar: %s " % np.mean(bin_avg)
+    nbar = Nbar/(bin_size**2)
+    print "average nbar: %s " % np.mean(nbar)
 
     tot_corr = sum(coadd_corr)
     tot_corr = [i/len(coadd_corr) for i in tot_corr]
@@ -109,15 +143,13 @@ def twoD_ps(coords,bns=20,rnge=100,shift=0,show_nr=False,show_ps=False):
         plt.colorbar()
         py.show()
 
-    coadd_ps = [i/len(coadd_ps) for i in coadd_ps]
-    tot_ps = sum(coadd_ps)
+    individual_ps = [i/len(individual_ps) for i in individual_ps]
+    tot_ps = sum(individual_ps)
     tot_ps = np.fft.fftshift(tot_ps)
     kx = 2*np.pi*np.fft.fftfreq(tot_ps.shape[0],d=bin_size)
     kx = np.fft.fftshift(kx)
     ky = 2*np.pi*np.fft.fftfreq(tot_ps.shape[1],d=bin_size)
     ky = np.fft.fftshift(ky)
-    #print kx
-    #print [2*np.pi/i for i in kx]
 
     if show_ps == True:
         py.figure('2d Power Spectrum')
@@ -125,17 +157,26 @@ def twoD_ps(coords,bns=20,rnge=100,shift=0,show_nr=False,show_ps=False):
         plt.colorbar()
         py.show()
 
-    return coadd_ps,tot_ps,kx,ky,bin_size
+    return individual_ps,tot_ps,kx,ky,bin_size
 
-def oneD_ps(data,kx,ky,rnge=100,bin_size=1,pixel_size=1):
+def angular_average(data,kx,ky,rnge=100,bin_size=1,dk=1):
+    """
+    takes in a 2d map and returns a 1d, angularly-averaged map
+    inputs:
+        data: 2d map
+        ky,ky: fft frequencies
+        rnge: 2d map range
+        bin_size:
+        pixel_size:
+
+    """
     ps2d = np.array(data)
     x,y = np.meshgrid(ky,kx)
     k = np.sqrt(x**2+y**2)
-    kmax = k.max()
-    dk = pixel_size
+    kmax = max(kx)
+
+    dk = dk
     K = np.arange(kmax/dk)*dk
-    """print K
-    print [2*np.pi/i for i in K]"""
 
     ps1d = []
     for i in range(len(K)):
@@ -148,20 +189,39 @@ def oneD_ps(data,kx,ky,rnge=100,bin_size=1,pixel_size=1):
     A_pix = bin_size**2
     A_box = rnge**2
     norm = A_pix**2/A_box
-    ps1d = [norm*i for i in ps1d]
-
+    ps1d = [norm*i for i in ps1d][1:]
+    K = K[1:]
     return ps1d,K,norm
 
-def error_bars(coadd_ps,ps1d,kx,ky,rnge=100,pix_size=1):
+def variance(individual_ps,ps1d,N,kx,ky,rnge=100,pix_size=1):
+    """
+    takes in a list of lists where each nested list is a single 2d map and returns the variance of each map with respect to the average of all the maps
+        individual_ps: list of lists where each nested list is a single 2d map
+        ps1d: average of all 2d maps
+        ky,ky: fft frequencies
+        rnge: spatial extent out to which we want to consider subhalos
+        pix_size: pixel size
+    """
     var = []
-    for i in coadd_ps:
-        ps,kk,norm = oneD_ps(i,kx,ky,rnge=100,bin_size=1,pixel_size=pix_size)
-        ps = [norm*i for i in ps]
+    for i in individual_ps:
+        ps,kk,norm = angular_average(i,kx,ky,rnge=100,bin_size=1,dk=pix_size)
         variance = [(i-j)**2 for i,j in zip(ps,ps1d)]
         var.append(variance)
+    x = 1/N
+    var = x*np.sum(var,0)
+    return var
 
-    x = 1/len(var)
-    #vari = [x*sum(i) for i in zip(*var)]
-    error = x*np.sum(var,0)
-
-    return error
+def poisson_realization(rnge=100,subh_num=1,num_proj=3000):
+    """
+    Creates 2d maps with a given number of randomly distributed subhalos
+        rnge: box size
+        subh_num: desired number of subhalos in the map
+        num_proj: desired number of projections, default is 3000
+    """
+    coords = []
+    count = 0
+    while count < num_proj:
+        count += 1
+        rand = np.random.uniform(-rnge/2.,rnge/2.,(int(subh_num),2))
+        coords.append(rand)
+    return coords
